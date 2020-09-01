d9395ae918316a36fee55f3d5f569462a59e3b19
gitRajith <67183785+gitRajith@users.noreply.github.com>
2020-09-01 20:54:46 +0530

    Add react-router-dom packages & Complete (Navbar,Footer,Home,Laptop,Desktop,Accessories,Contactus,Userreg)jsx-V1.2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import React, { Component } from "react";
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './components/Navbar';
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Route } from "react-router-dom";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
+
+import NavBar from "./components/Navbar";
+import Home from "./components/Home";
+import Laptop from "./components/Laptop";
+import Desktop from "./components/Desktop";
+import Accessories from "./components/Accessories";
+import Contactus from "./components/Contactus";
+import Userreg from "./components/Userreg";
+import Login from "./components/Login";
+import Product from "./components/Product";
 import Footer from './components/Footer';
 
-class App extends Component {
-  render(){
 
-  return (
-    <div className="App">
-      <Navbar/>
-      <br/>
-      This is sample text
-      <br/>
-      <Footer/>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <NavBar />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/Laptop" component={Laptop} />
+          <Route exact path="/Desktop" component={Desktop} />
+          <Route exact path="/Accessories" component={Accessories} />
+          <Route exact path="/Contactus" component={Contactus} />
+          <Route exact path="/Userreg" component={Userreg} />
+          <Route exact path="/product/:id" component={Product} />
+          <Route exact path="/user-login" component={Login} />
+          <Route exact path="/product/:id" component={Product} />
+          <Route exact path="/user-login" component={Login} />
+
+          <Footer />
+        </BrowserRouter>
+      </div>
+    );
   }
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,36 +1,17 @@
-//import React, { Component } from "react";
-import logo from './logo.svg';
-import './App.css';
-//import 'bootstrap/dist/css/bootstrap.min.css';
-//import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter, Route } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.css";
-//import Heroes from "./components/Heroes";
-import NavBar from "./components/Navbar";
-import Home from "./components/Home";
-import Laptop from "./components/Laptop";
-import Desktop from "./components/Desktop";
-import Accessories from "./components/Accessories";
-import Contactus from "./components/Contactus";
-import Userreg from "./components/Userreg";
-//import HeroDetails from "./components/HeroDetails";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <NavBar />
-    <Route exact path="/" component={Home} />
-    <Route exact path="/Laptop" component={Laptop} />
-    <Route exact path="/Desktop" component={Desktop} />
-    <Route exact path="/Accessories" component={Accessories} />
-    <Route exact path="/Contactus" component={Contactus} />
-    <Route exact path="/Userreg" component={Userreg} />
-    
-    <Footer/>
-    
-  </BrowserRouter>,
-  document.getElementById("root")
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
