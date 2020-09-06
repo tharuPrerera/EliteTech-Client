015c1d23f29fcd5e6624fad3290e89785d819f32
Dhanaraj Ravichandran <49708330+DhanaRavichandran@users.noreply.github.com>
2020-09-06 10:04:44 +0530

    Add minor fixes

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -36,187 +36,79 @@ tabRow() {
     //     return<cartRow obj={object} key={i}/>
     // });
   }
-
-    render() {
+render() {
         return (
-            <div>
-            <div >
-                <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-                    <a className="navbar-brand text-white text-camelcase" href="#">
-                        <h1> EliteTech</h1>
-                    </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav1" aria-controls="basicExampleNav1" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                    {/* <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>*/}
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <form class="form-inline my-2 my-lg-0">
-                        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                    </form>
-                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <nav class="navbar navbar-expand-md navbar-light">
-                     <div class="collapse navbar-collapse" id="basicExampleNav1">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item">
-                        <a a className="navbar-brand text-white text-camelcase" href="#">
-                            <span class="badge badge-pill red">1</span>
-                            <i class="fas fa-shopping-cart pl-0"></i>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a className="navbar-brand text-white text-camelcase" href="#">Login</a>
-                    </li>
-                </ul>
-            </div>
-    </nav>
-     </div>
-                </nav> 
-                </div>
+               <div >
                  <div>
                       <table className="table table-striped" style={{marginTop:20}}>
+                          
                           <thead>
-                              <tr>
-                                <th>Item</th>
-                                <th>Quantity</th>
-                                <th>Unit Price</th>
-                                <th colSpan="3">Action</th>
-                             </tr>
+                            <tr>
+                              <th>Item</th>
+                              <th>Quantity</th>
+                              <th>Unit Price</th>
+                              <th colSpan="3">Action</th>
+                            </tr>
                           </thead>
+                          
                           <tbody>
-		    <tr>
-		     
-		      <td class="w-25">
-           
-      <div >
-        <img class="img-fluid w-100"
-          src="https://computing.which.co.uk/hc/article_attachments/360003986679/mac.JPG"/>
-        <div class="mask rgba-black-slight"></div>
-      </div>
-    
+                            <tr>
+                              
+                              <td class="w-25">
+                                <div >
+                                   <img class="img-fluid w-100" src="https://computing.which.co.uk/hc/article_attachments/360003986679/mac.JPG"/>
+                                <div class="mask rgba-black-slight"></div>
+                                </div>
+                              </td>
+                            
+                              <td class="w-25">
+                                <div>
+                                 <button onClick={this.doDecrement} className="fa fa-minus fa-inverse fa-2x"></button>
+                                 <input type="text" className="number" value={this.state.value }></input>
+                                 <button onClick={this.doIncrement} className="fa fa-plus fa-inverse fa-2x"></button>
+                                 <sub>{this.state.message}</sub>
+                                </div>
+                              </td>
+                            
+                             <td>Rs350,000.00</td>
+                             
+                             <td>
+                               <button className="btn btn-danger">Delete</button></td>
+                            </tr>
 
- 
-		      </td>
-              <td class="w-25">
-                {/*<div class="card-body text-center">
-
-        <p><h5>Acer Nitro 5 Core i7 8750H Gaming Laptop - Windows 10</h5></p>
-        <ul class="rating">
-        
-            <i class="fas fa-star fa-sm text-primary"></i>
-            <i class="fas fa-star fa-sm text-primary"></i>
-            <i class="fas fa-star fa-sm text-primary"></i>
-            <i class="fas fa-star fa-sm text-primary"></i>
-            <i class="far fa-star fa-sm text-primary"></i>
-
-        </ul>
-
-        <h6 class="mb-3">
-        <span class="text-danger mr-1">Rs240,000.00</span>
-                </h6>*/}
-                <div>
-                <button onClick={this.doDecrement} className="fa fa-minus fa-inverse fa-2x"></button>
-                <input type="text" className="number" value={this.state.value }></input>
-                <button onClick={this.doIncrement} className="fa fa-plus fa-inverse fa-2x"></button>
-                <sub>{this.state.message}</sub>
-                </div>
-            
-
-    {/*<button type="button" class="btn btn-primary btn-sm mr-1 mb-2">
-    <i class="fas fa-shopping-cart pr-2"></i>Add to cart
-    </button>
-    <button type="button" class="btn btn-light btn-sm mr-1 mb-2">
-    <i class="fas fa-info-circle pr-2"></i>Details
-    </button>
-    <button type="button" class="btn btn-danger btn-sm px-3 mb-2 material-tooltip-main" data-toggle="tooltip" data-placement="top" title="Add to wishlist">
-    <i class="far fa-heart"></i>
-    </button>
-            
-            </td>
-    </div>
-    */}
-    </td>
-    <td>Rs350,000.00</td>
-	<td><button class="btn"><i class="fa fa-trash"></i></button> </td>
-    </tr>
-    <tr>
-    <td class="w-25">
-           
-           <div class="mask">
-             <img class="img-fluid w-100"
-               src="https://cdn.takas.lk/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/3/7/37_2_9_1.jpg"/>
-             <div class="mask rgba-black-slight"></div>
-           </div>
-         
-     
-      
-                   </td>
-                   <td>
-                     {/*<div class="card-body text-center">
-     
-             <p><h5>Acer Nitro 5 Core i7 8750H Gaming Laptop - Windows 10</h5></p>
-             <ul class="rating">
-             
-                 <i class="fas fa-star fa-sm text-primary"></i>
-                 <i class="fas fa-star fa-sm text-primary"></i>
-                 <i class="fas fa-star fa-sm text-primary"></i>
-                 <i class="fas fa-star fa-sm text-primary"></i>
-                 <i class="far fa-star fa-sm text-primary"></i>
-     
-             </ul>
-     
-             <h6 class="mb-3">
-             <span class="text-danger mr-1">Rs240,000.00</span>
-                     </h6>*/}
-                     <div>
-                     <button onClick={this.doDecrement} className="fa fa-minus fa-inverse fa-2x"></button>
-                     <input type="text" className="number" value={this.state.value }></input>
-                     <button onClick={this.doIncrement} className="fa fa-plus fa-inverse fa-2x"></button>
-                     <br/>
-                     <sub>{this.state.message}</sub>
-                     </div>
-                 
-     
-         {/*<button type="button" class="btn btn-primary btn-sm mr-1 mb-2">
-         <i class="fas fa-shopping-cart pr-2"></i>Add to cart
-         </button>
-         <button type="button" class="btn btn-light btn-sm mr-1 mb-2">
-         <i class="fas fa-info-circle pr-2"></i>Details
-         </button>
-         <button type="button" class="btn btn-danger btn-sm px-3 mb-2 material-tooltip-main" data-toggle="tooltip" data-placement="top" title="Add to wishlist">
-         <i class="far fa-heart"></i>
-         </button>
-                 
-                 </td>
-         </div>
-         */}
-         </td>
-         <td>Rs240,000.00</td>
-         <td><button class="btn"><i class="fa fa-trash"></i></button></td>
-         </tr>
-    </tbody>
+                            <tr>
+                              
+                              <td class="w-25">
+                                <div >
+                                   <img class="img-fluid w-100" src="https://computing.which.co.uk/hc/article_attachments/360003986679/mac.JPG"/>
+                                <div class="mask rgba-black-slight"></div>
+                                </div>
+                              </td>
+                            
+                              <td class="w-25">
+                                <div>
+                                 <button onClick={this.doDecrement} className="fa fa-minus fa-inverse fa-2x"></button>
+                                 <input type="text" className="number" value={this.state.value }></input>
+                                 <button onClick={this.doIncrement} className="fa fa-plus fa-inverse fa-2x"></button>
+                                 <sub>{this.state.message}</sub>
+                                </div>
+                              </td>
+                            
+                             <td>Rs350,000.00</td>
+                             
+                             <td>
+                               <button className="btn btn-danger">Delete</button></td>
+                            </tr>
+                          
+                          </tbody>
                       </table>
-                      <button type="button" class="btn btn-dark">
-                          Confirm & Pay
-         </button>
-         <br/>
-         <br/>
-
-                      <button type="button" class="btn btn-dark">
-         <i class="fa fa-arrow-left"></i> Continue Shopping
-         </button>
-         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-
-         <button type="button" class="btn btn-dark">
-         Next <i class="fa fa-angle-right"></i>
-         </button>
-                      </div>
-            </div>
-        );
-                
+                      
+                      <button type="button" class="btn btn-dark">Confirm & Pay</button>
+                      {" "}
+                      <button type="button" class="btn btn-dark">Continue Shopping <i class="fa fa-arrow-right"></i></button><br/><br/>
+                    </div>
+                </div>
+    );
     }
     doDecrement=() =>{
         if(this.state.value) {
@@ -242,7 +134,6 @@ tabRow() {
           });
         }
     }
-        
-    
-}
-export default cart;
+ }
+
+ export default cart;
---
+++ src/components/Footer.js
@@ -0,0 +1,84 @@
+import React from "react";
+import styled from "styled-components";
+
+function Footer() {
+  return (
+    <FooterContainer className="main-footer">
+      <div className="footer-middle">
+        <div className="container">
+          <div className="row">
+
+                {/*Column 1*/}
+                <div className="col-md-3 col-sm-6">
+                <ul className="list-unstyled">
+                    <h4>Laptop</h4>
+                    <li>Lorem ipsum</li>
+                    <li>Lorem ipsum</li>
+                    <li>Lorem ipsum</li>
+                    <li>Lorem ipsum</li>
+                </ul>
+                </div>
+
+                {/*Column 2*/}
+                <div className="col-md-3 col-sm-6">
+                <ul className="list-unstyled">
+                    <h4>Desktop</h4>
+                    <li>Lorem ipsum</li>
+                    <li>Lorem ipsum</li>
+                    <li>Lorem ipsum</li>
+                    <li>Lorem ipsum</li>
+                </ul>
+                </div>
+
+                {/*Column 3*/}
+                <div className="col-md-3 col-sm-6">
+                <ul className="list-unstyled">
+                    <h4>Accessories</h4>
+                    <li>Lorem ipsum</li>
+                    <li>Lorem ipsum</li>
+                    <li>Lorem ipsum</li>
+                    <li>Lorem ipsum</li>
+                </ul>
+                </div>
+
+                {/*Column 4*/}
+                <div className="col-md-3 col-sm-6">
+                <ul className="list-unstyled">
+                    <h4>Contact Us</h4>
+                    <li>Lorem ipsum</li>
+                    <li>Lorem ipsum</li>
+                    <li>Lorem ipsum</li>
+                    <li>Lorem ipsum</li>
+                </ul>
+                </div>
+
+
+            </div>
+            {/*Footer Bottom*/}
+            <div className="footer-bottom">
+              <p className="text-xs-center">
+                &copy;{new Date().getFullYear()}EliteTech -All Rights Reserved
+              </p>
+            </div>
+
+
+
+          </div>
+        </div>
+    </FooterContainer>
+  );
+}
+export default Footer;
+
+const FooterContainer = styled.footer`
+  .footer-middle {
+    background-color:#3a4040;
+    padding-top: 3rem;
+    color:#fff;
+  }
+
+  .footer-bottom{
+    padding-top: 3rem;
+    padding-bottom: 2rem;
+  }
+`;
