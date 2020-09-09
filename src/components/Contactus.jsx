ebb9e6dfdf0b11d10308ca2f6a3747bcdebaeded
gitRajith <67183785+gitRajith@users.noreply.github.com>
2020-09-09 06:28:50 +0530

    Added third party API(mail sending)  with authentication( to contact us page)-V1.1
    Added third party API(mail sending)  with authentication( to contact us page)-V1.1

--- src/components/Contactus.jsx
+++ src/components/Contactus.jsx
@@ -13,30 +13,57 @@ import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
 // //currency exchange
 // import {ExchangeRate} from "react-currency-conversion";
 
+//email sender import
+import emailjs from "emailjs-com";
+
+
 const mapStyles = {
   width: "60%",
   height: "90%",
 };
 
+function sendEmail(e) {
+  e.preventDefault();
+
+  emailjs.sendForm('gmail', 'template_kb8gj4e', e.target, 'user_7EutIespHBk5qq0zVokN0')
+    .then((result) => {
+        console.log(result.text);
+    }, (error) => {
+        console.log(error.text);
+    });
+    e.target.reset();
+}
+
 class Contactus extends Component {
+
   render() {
     return (
       <>
         <div class="row">
           <div class="col-md-2 col-md-offset-4"></div>
 
-          <Form>
+          <Form onSubmit={sendEmail}>
+            <Form.Group controlId="exampleForm.ControlInput1">
+              <Form.Label>Name</Form.Label>
+              <Form.Control type="text" placeholder="Name" name="name" />
+            </Form.Group>
             <Form.Group controlId="exampleForm.ControlInput1">
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="name@example.com" />
+              <Form.Control type="email" placeholder="Email Address" name="email" />
+            </Form.Group>
+            <Form.Group controlId="exampleForm.ControlInput1">
+              <Form.Label>Subject</Form.Label>
+              <Form.Control type="text" placeholder="Subject" name="subject" />
             </Form.Group>
             <Form.Group controlId="exampleForm.ControlTextarea1">
-              <Form.Label>Your Info</Form.Label>
-              <Form.Control as="textarea" rows="3" />
+              <Form.Label>Message Here</Form.Label>
+              <Form.Control as="textarea" rows="3" name="message" />
             </Form.Group>
-            <Button variant="primary">Send</Button>{" "}
-          </Form>
+           
+            <input type ="submit" value="Send Message" className = "btn btn-primary"/>
 
+          </Form>
+    
           <div class="col-md-8 col-sm-15">
             {/* <h4>123</h4> */}
             &nbsp;
@@ -52,61 +79,27 @@ class Contactus extends Component {
               <Marker onClick={this.onMarkerClick} name={"Current location"} />
 
               <InfoWindow onClose={this.onInfoWindowClose}>
-                {/* 
+                  {/* 
                       <div>
                         <h1>{this.state.selectedPlace.name}</h1>
                       </div>
                   */}
               </InfoWindow>
             </Map>
-
-            
             &nbsp;
           </div>
-          
         </div>
-
-
-        <div className="col-md-2 col-sm-15" >
-        {/* <div style={{ marginTop: 50 }} align="right">
+        <div className="col-md-2 col-sm-15">
+          {/* <div style={{ marginTop: 50 }} align="right">
           <h1>hi</h1>
         </div> */}
-        {/* <h5>hi</h5> */}
+          {/* <h5>hi</h5> */}
         </div>
         &nbsp;&nbsp;
-
-       
       </>
     );
 
-    // return (
-    //   <>
-    //     <div class="row">
-    //       <div class="col-md-2 col-md-offset-4"></div>
-
-    //       <Form>
-    //         <Form.Group controlId="exampleForm.ControlInput1">
-    //           <Form.Label>Email address</Form.Label>
-    //           <Form.Control type="email" placeholder="name@example.com" />
-    //         </Form.Group>
-    //         <Form.Group controlId="exampleForm.ControlTextarea1">
-    //           <Form.Label>Your Info</Form.Label>
-    //           <Form.Control as="textarea" rows="3" />
-    //         </Form.Group>
-    //         <Button variant="primary">Send</Button>{" "}
-    //       </Form>
-    //     </div>
-
-    //     <div class="row">
-    //       <div class="col-md-2 col-md-offset-4"></div>
-    //       <h4></h4>
-    //     </div>
-    //   </>
-    // );
-
     
-  
-
   }
 }
 
